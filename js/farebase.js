543e471033220ca9a9aa23b5a0d5195d573772a6
MichelaGhezziA <mgacortona@gmail.com>
2019-03-23 09:47:01 +0100

    first commit

---
+++ js/farebase.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyBAqlZqtKj1SaBzL3oAnhtizxTQfSKQKNc",
+    authDomain: "recepies-2dfb3.firebaseapp.com",
+    databaseURL: "https://recepies-2dfb3.firebaseio.com",
+    projectId: "recepies-2dfb3",
+    storageBucket: "recepies-2dfb3.appspot.com",
+    messagingSenderId: "529971312932"
+  };
+  firebase.initializeApp(config);
