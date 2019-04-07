39671485e03e7a4d4ace34b10b998550995d7c55
MichelaGhezziA <mgacortona@gmail.com>
2019-04-07 22:40:16 +0200

    first commit

---
+++ js/models/recepie.js
@@ -0,0 +1,14 @@
+    
+var recepie = {
+    ingredients: {},
+    name: "",
+    description: "",
+    images: {},
+    people: "",
+    preparationTime: "",
+    cookingTime: "",
+    difficulty: 0,
+    cost: 0,
+    rating: 0,
+    numVotes: 0
+};
